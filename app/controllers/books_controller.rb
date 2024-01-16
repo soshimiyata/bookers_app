ff9d126b25a3d029afb49f90bbc36713500739a7
SoshiMiyata <155513230+soshimiyata@users.noreply.github.com>
2024-01-16 16:10:49 +0000

    Rspec clear

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,11 +7,15 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
     flash[:notice] = "Book was successfully created."
-    redirect_to book_path(book.id)
-    flash[:notice] = 
+    redirect_to book_path(@book.id)
+    else
+    @books = Book.all
+    render :index
+    end
   end
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -23,9 +27,13 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated"
+      redirect_to book_path(@book.id)
+    else
+      render :show
+    end
   end
   
   def destroy
